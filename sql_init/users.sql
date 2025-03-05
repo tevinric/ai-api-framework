03e8cd1347dcd216f7612b52c1ebd122d424ced3
tevinric <tevinric@gmail.com>
2025-03-05 19:31:41 +0200

    updates

--- sql_init/users.sql
+++ sql_init/users.sql
@@ -6,6 +6,8 @@ CREATE TABLE users (
     user_name NVARCHAR(100) NOT NULL,
     user_email NVARCHAR(255) NOT NULL UNIQUE,
     common_name NVARCHAR(100),
+    company NVARCHAR(255),            -- Company name
+    department NVARCHAR(255),         -- Department name
     api_key UNIQUEIDENTIFIER NOT NULL UNIQUE DEFAULT NEWID(),
     scope INT CHECK (scope IN (0,1,2,3,4,5)) DEFAULT 1,
     active BIT NOT NULL DEFAULT 1,
