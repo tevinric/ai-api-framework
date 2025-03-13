82f584c05326dad3c781ab108f54bb34a5c818fb
Tevin <78318984+tevinric@users.noreply.github.com>
2025-03-13 20:48:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -163,6 +163,9 @@ register_speech_to_text_diarize_routes(app)
 from apis.document_intelligence.summarization import register_document_intelligence_routes
 register_document_intelligence_routes(app)
 
+from apis.ocr.sa_id import register_sa_id_ocr_routes
+register_sa_id_ocr_routes(app)
+
 if __name__ == '__main__':
     app.run(debug=True)
     
