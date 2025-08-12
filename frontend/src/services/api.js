765df7e9414a37b412f9d8e0320d4e304c088fe7
Tevin Richard <tevinri@tihsa.co.za>
2025-08-12 20:44:44 +0200

    updates

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://gaia.com';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://dev-api.tihsa.co.za/ext/api/v1/gaia';
 
 class ApiService {
   constructor() {
