fddc72719fb91398b06ae449b29ee389f8bc9692
Tevin <tevinric@gmail.com>
2025-08-13 08:41:05 +0200

    updates

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { isAuthenticated } from '../utils/auth';
-
-const ProtectedRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
